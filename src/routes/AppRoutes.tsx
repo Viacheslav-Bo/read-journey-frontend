4b23aba7b52f0e8bb6971b3eeaaf6b449e60bf1d
Viacheslav-Bo <zotikivna@gmail.com>
2026-07-31 16:17:35 +0300

    chore: update AppRoutes

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,5 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import ProtectedRoute from "./ProtectedRoute";
+import RegisterPage from "../pages/auth/RegisterPage";
+import LoginPage from "../pages/auth/LoginPage";
 
 const AppRoutes = () => {
   return (
@@ -7,9 +9,9 @@ const AppRoutes = () => {
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
       <Route element={<ProtectedRoute />}>
-        <Route path="/recommended" element={<RecommendedPage />} />
-        <Route path="/library" element={<LibraryPage />} />
-        <Route path="/reading/:bookId" element={<ReadingPage />} />
+        {/* <Route path="/recommended" element={<RecommendedPage />} /> */}
+        {/* <Route path="/library" element={<LibraryPage />} /> */}
+        {/* <Route path="/reading/:bookId" element={<ReadingPage />} /> */}
       </Route>
     </Routes>
   );
